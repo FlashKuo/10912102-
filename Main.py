f0ce51cc0098c5c8a6ebe3c2edb477ecf6b33b8d
FlashKuo <79870503+FlashKuo@users.noreply.github.com>
2021-03-03 17:58:36 +0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,12 @@
+from myMath import myArithmetic,myCalcArea,myStatistics
+the_first_number=float(input('input your first number:'))
+the_second_number=float(input('input your second number:'))
+the_third_number=float(input('input your third number:'))
+the_fourth_number=float(input('input your fourth number:'))
+the_fifth_number=float(input('input your fifth number:'))
+the_first_plus_the_second_number=myArithmetic.myAdd(the_first_number,the_second_number)
+the_third_plus_the_fourth_number=myArithmetic.myAdd(the_third_number,the_fourth_number)
+the_first_plus_the_second_plus_the_third_plus_the_fourth_number=myArithmetic.myAdd(the_first_plus_the_second_number,the_third_plus_the_fourth_number)
+total_five_number=myArithmetic.myAdd(the_first_plus_the_second_plus_the_third_plus_the_fourth_number,the_fifth_number)
+total_five_number_divise_five=myArithmetic.myDiv(total_five_number,5)
+print(total_five_number_divise_five)
